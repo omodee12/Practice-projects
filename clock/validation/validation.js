fe1ff35b8c225d076aa20c9306bbd6cb9e059ece
unknown <omodara.tobi@gmail.com>
2021-11-10 15:58:49 +0100

    validation

---
+++ clock/validation/validation.js
@@ -0,0 +1,12 @@
+const name = document.getElementById('name')
+const password = document.getElementById('password')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('error')
+
+form.addEventListener('submit', (e) => {
+    let messages = []
+    if (name.value === '' || name.value == null) {
+        messages.push('name is required')
+    }
+    e.preventDefault()
+})
